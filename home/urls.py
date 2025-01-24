0ee1e2a11a11a2648e947cf29c0754befc6712f2
Rarysson <raryssongui@gmail.com>
2025-01-24 20:47:25 -0300

    Atividade 12 - Templates e Links

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sobre, name='home'),
+    path('', views.index, name='index'),  # Página inicial
+    path('contato/', views.contato, name='contato'),  # Página de contato
     path('sobre/', views.sobre, name='sobre'),
-    path('contato/', views.contato, name='contato'),
     path('ajuda/', views.ajuda, name='ajuda'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,14 @@
+# home/views.py
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
-def sobre(request):
-    return HttpResponse("<h1>Sobre</h1><p>Informações sobre o sistema.</p>")
+def index(request):
+    return render(request, 'index.html')
 
 def contato(request):
-    return HttpResponse("<h1>Contato</h1><p>Esta é a página de contato.</p>")
+    return render(request, 'contato.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
 
 def ajuda(request):
-    return HttpResponse("<h1>Ajuda</h1><p>Esta é a página de ajuda.</p>")
+    return render(request, 'ajuda.html')
