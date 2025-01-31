b0a8579392950ccec39296f2dfe1c6796cba151d
Raryy00 <160739029+Raryy00@users.noreply.github.com>
2025-01-31 10:17:09 -0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,3 @@
-# home/views.py
 from django.shortcuts import render
 
 def index(request):
@@ -15,6 +14,22 @@ def ajuda(request):
 
 def local(request):
     return render(request, 'local.html')
-
+    
 def exibiritem(request, id):
     return render(request, 'exibiritem.html', {'id': id})
+
+def perfil(request, usuario):
+    return render(request, 'perfil.html', {'usuario': usuario})
+
+def dia_da_semana(request, num):
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado"
+    }
+    dia = dias.get(num, "Número inválido! Escolha um número entre 1 e 7.")
+    return render(request, 'dia.html', {'dia': dia})
