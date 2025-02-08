c02d4ad03815bdc18e108f993c78e06977c55a29
Raryy00 <160739029+Raryy00@users.noreply.github.com>
2025-02-08 11:17:11 -0300

    Update urls.py

--- pweb/urls.py
+++ pweb/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from alunos.views import listar_alunos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', include('alunos.urls')),
+    path('', include('home.urls')),
 ]
